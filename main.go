dc14340a0d842de6952911c216a67c590f3d939d
Allan Dickow <amdickow@gmail.com>
2019-11-12 21:05:25 +0100

    Add support for defining host/ip for the service to run healthcheck against

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
         port := flag.String("port", "80", "port on localhost to check")
+        host := flag.String("host", "127.0.0.1", "host to run the check against")
         flag.Parse()
 
-        resp, err := http.Get("http://127.0.0.1:" + *port + "/health")
+        resp, err := http.Get("http://" + *host + ":" + *port + "/health")
         if err != nil || resp.StatusCode != 200 {
                 os.Exit(1)
         }
